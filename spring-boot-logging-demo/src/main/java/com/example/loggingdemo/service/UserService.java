6fc6a66156d5cc339f88944693fdf530f32a25df
manickmanick <manickaperumal22@gmail.com>
2026-09-14 16:06:49 +0530

    Configure Logback with custom patterns and logger levels

--- spring-boot-logging-demo/src/main/java/com/example/loggingdemo/service/UserService.java
+++ spring-boot-logging-demo/src/main/java/com/example/loggingdemo/service/UserService.java
@@ -11,8 +11,13 @@ public class UserService {
             LoggerFactory.getLogger(UserService.class);
 
     public String getUser(String name){
-        log.debug("Looking up user. name={}",name);
-        log.info("user lookup completed. name={}",name);
+//        log.debug("Looking up user. name={}",name);
+//        log.info("user lookup completed. name={}",name);
+        log.trace("TRACE message");
+        log.debug("DEBUG message");
+        log.info("INFO message");
+        log.warn("WARN message");
+        log.error("ERROR message");
         return "user: " + name;
     }
 }
