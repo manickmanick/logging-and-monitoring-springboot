ffab13fedd55e447eec4aefda156280a74f142f3
manickmanick <manickaperumal22@gmail.com>
2026-09-15 18:01:20 +0530

    Add custom Micrometer metrics with MeterRegistry and Counter

--- spring-boot-logging-demo/src/main/java/com/example/loggingdemo/controller/UserController.java
+++ spring-boot-logging-demo/src/main/java/com/example/loggingdemo/controller/UserController.java
@@ -1,23 +1,41 @@
 package com.example.loggingdemo.controller;
 
 import com.example.loggingdemo.service.UserService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
+    private final Counter userCreatedCounter;
+
+    public UserController(MeterRegistry meterRegistry,UserService userService) {
+        this.userCreatedCounter = Counter.builder("users.created")
+                .description("Number of users created")
+                .register(meterRegistry);
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
     public String getUser(@RequestParam String name){
         log.info("Received request to get User. name={}",name);
         return userService.getUser(name);
+    }
+
+    @PostMapping("/users")
+    public String createUser() {
+
+        userCreatedCounter.increment();
+
+        return "User created";
     }
 }
